cb2d435835ca0a5a53fadf720fd9a30c24dd4e5d
Sapun Jyoti Gogoi <149178248+sapunjgogoi@users.noreply.github.com>
2026-02-28 23:35:01 +0530

    A code for Palindrome number to find
    Added user input functionality using Scanner.

---
+++ palindromeNumber.java
@@ -0,0 +1,28 @@
+// LeetCode 9 
+
+class palindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x = x / 10;
+        }
+
+        return (x == reversedHalf) || (x == reversedHalf / 10);
+    }
+
+    public static void main(String[] args) {
+      // User Scanner class for user input
+        System.out.println(isPalindrome(121));   // true
+        System.out.println(isPalindrome(-121));  // false
+        System.out.println(isPalindrome(10));    // false
+    }
+}
